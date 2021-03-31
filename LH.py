72aceafea02b536d02f350cc3c63207b0262c134
mosliu <lx0319@gmail.com>
2021-03-31 10:32:50 +0800

    update to multi account

--- LH.py
+++ LH.py
@@ -7,7 +7,7 @@ from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.lighthouse.v20200324 import lighthouse_client, models
-
+# 20210331 修改
 SecretId = os.environ["SecretId"]
 SecretKey = os.environ["SecretKey"]
 # region = os.environ["region"]
